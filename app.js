508180d4534b2bc8761a22746a3be531721b08a3
unknown <michaelcmatteis@gmail.com>
2024-12-10 20:24:13 -0600

    adding

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRoutes = require("./routes/usersRoutes")
 let port = process.env.PORT || 3000
 
 
-app.use(cors())
+app.use(cors({
+  origin:"http://localhost:5173/"
+}))
 app.use(express.json())
 
 app.get('/',function(req,res){
